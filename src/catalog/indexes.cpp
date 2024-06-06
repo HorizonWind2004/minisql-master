ae8d646a25db08567dcc914ee1131967a41fbe9a
whql251 <59035039+whql251@users.noreply.github.com>
2024-06-06 18:39:55 +0800

    Update on framework

--- src/catalog/indexes.cpp
+++ src/catalog/indexes.cpp
@@ -98,7 +98,16 @@ uint32_t IndexMetadata::DeserializeFrom(char *buf, IndexMetadata *&index_meta) {
 
 Index *IndexInfo::CreateIndex(BufferPoolManager *buffer_pool_manager, const string &index_type) {
   size_t max_size = 0;
+  uint32_t column_cnt = key_schema_->GetColumns().size();
+  size_t size_bitmap = (column_cnt % 8) ? column_cnt / 8 + 1 : column_cnt / 8;
+  // rid + column_cnt + bitmap
+  max_size += 8 + 4 + sizeof(unsigned char) * size_bitmap;
+
   for (auto col : key_schema_->GetColumns()) {
+    // length of char column
+    if(col->GetType() == TypeId::kTypeChar)
+      max_size += 4;
+
     max_size += col->GetLength();
   }
 
